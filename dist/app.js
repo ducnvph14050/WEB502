eacd16eea3db9ff6b44e957b34001871dd31f663
VanDuc <ducnvph14050@fpt.edu.vn>
2022-03-09 14:17:08 +0700

    first commit

---
+++ dist/app.js
@@ -0,0 +1,22 @@
+"use strict";
+//vd1:
+//function sum(a: number, b:number){
+//   return a+b;
+//}
+//const result = sum(10, 20)
+//console.log(result);
+//vd2:
+// let number1: number = 5;
+// let number2: number = 2.8;
+// let phrase: string = 'Result is';
+// let permit: boolean = true;
+const person = {
+    id: 1,
+    name: "Duc",
+    age: 21
+};
+const persons = [
+    { id: 1, name: "Duc", age: 21, status: true },
+    { id: 2, name: "Tung", age: 20, status: true },
+];
+console.log(persons);
